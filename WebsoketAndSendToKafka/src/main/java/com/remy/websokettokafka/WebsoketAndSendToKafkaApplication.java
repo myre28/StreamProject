aa1ab54c4563fabca28d51f062510fc57ebfc7c7
Bwamutala Miantezila <bwamutalamiantezila@MacBook-Pro.attlocal.net>
2020-11-09 21:27:17 -0800

    modified websockettokafka

--- WebsoketAndSendToKafka/src/main/java/com/remy/websokettokafka/WebsoketAndSendToKafkaApplication.java
+++ WebsoketAndSendToKafka/src/main/java/com/remy/websokettokafka/WebsoketAndSendToKafkaApplication.java
@@ -1,7 +1,6 @@
 package com.remy.websokettokafka;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
