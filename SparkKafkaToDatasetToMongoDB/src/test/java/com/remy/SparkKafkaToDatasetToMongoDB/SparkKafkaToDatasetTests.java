10c34ba58edeb96f65c56bc60632bd99d3306ec1
Bwamutala Miantezila <bwamutalamiantezila@MacBook-Pro.attlocal.net>
2020-11-09 21:52:02 -0800

    added SparkKafkaToDatasetToMongoDB

---
+++ SparkKafkaToDatasetToMongoDB/src/test/java/com/remy/SparkKafkaToDatasetToMongoDB/SparkKafkaToDatasetTests.java
@@ -0,0 +1,15 @@
+/*
+package com.remy.SparkKafkaToDatasetToMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkKafkaToDatasetToMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
