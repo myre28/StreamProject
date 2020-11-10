4460d9e0f6cec3a87abcaafab702750948d1f873
Bwamutala Miantezila <bwamutalamiantezila@MacBook-Pro.attlocal.net>
2020-11-09 20:26:53 -0800

    first commit2

---
+++ WebsoketAndSendToKafka/src/test/java/com/remy/websoketandsendtokafka/WebsoketAndSendToKafkaApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.remy.websoketandsendtokafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsoketAndSendToKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
